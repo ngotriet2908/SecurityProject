31897ec68978a828e2417b8b4be400e995025ec1
ngocapu <ngotriet2908@gmail.com>
2019-10-30 17:50:12 +0100

    - Remodel structure of packages

---
+++ src/main/java/Mod5/Mod5/Dao/ServerResources.java
@@ -0,0 +1,68 @@
+package Mod5.Mod5.Resouces;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import java.io.*;
+
+@Path("/security")
+public class ServerResources {
+    private static boolean beingInvaded = false;
+
+    @Context
+    ServletContext servletContext;
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_HTML)
+    public String hello() {
+        System.out.println("Hello");
+        return "Hello";
+    }
+
+    @GET
+    @Path("/status")
+    @Produces(MediaType.APPLICATION_JSON)
+    public boolean getStatus() {
+        System.out.println(ServerResources.beingInvaded);
+        return ServerResources.beingInvaded;
+    }
+
+    @POST
+    @Path("/update/{status}")
+    public void update(@PathParam("status") boolean status) {
+        ServerResources.beingInvaded = status;
+        System.out.println("UPDATE\nStatus: " + status + "\n" + ServerResources.beingInvaded);
+    }
+
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public InputStream showLoginPage(@Context HttpServletResponse response, @Context HttpServletRequest request) throws IOException {
+//        if (error != null) {
+//            if (error.equals("not_authorized") ||
+//                    error.equals("not_activated") ||
+//                    error.equals("reset_token_invalid")) {
+//                response.addHeader("error", error);
+//            }
+//        }
+//
+//        if (message != null) {
+//            if (message.equals("reset_success") ||
+//                    message.equals("reset_request_success") ||
+//                    message.equals("registration_success")) {
+//                response.addHeader("message", message);
+//            }
+//        }
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("../../html/index.html");
+
+        System.out.println("Req received.");
+
+        return inputStream;
+    }
+}
