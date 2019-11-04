8d9e7facc2ed6cb3135ed45f061c8c92c83c7667
crist <c.trusin@student.utwente.nl>
2019-11-04 18:20:23 +0100

    migrated to hosted DB

--- src/main/java/Mod5/Mod5/settings/DatabaseInitialiser.java
+++ src/main/java/Mod5/Mod5/settings/DatabaseInitialiser.java
@@ -32,12 +32,12 @@ public class DatabaseInitialiser implements ServletContextListener {
 //        String password = "123000";
 
 
-        String host = "localhost";
-        String dbName = "postgres";
+        String host = "cristiantrusin.ddns.net";
+        String dbName = "mod5";
         String myschema = "mod5";
         String url = "jdbc:postgresql://" + host + ":5432/" + dbName + "?currentSchema=" + myschema;
-        String user = "ngocapu";
-        String password = "";
+        String user = "member";
+        String password = "1q2w3e4r5t";
 
         try {
             con = DriverManager.getConnection(url, user, password);
